92db8941062136ca112d9b46821034eb3fc83c2e
Muhammadfraz98 <muhammadfraz98@gmail.com>
2025-10-27 10:33:07 +0100

    placed own model

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-/*
- * Copyright 2017 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-/**
- * Query for WebXR support. If there's no support for the `immersive-ar` mode,
- * show an error.
- */
 (async function() {
   const isArSessionSupported = navigator.xr && navigator.xr.isSessionSupported && await navigator.xr.isSessionSupported("immersive-ar");
   if (isArSessionSupported) {
@@ -66,11 +48,7 @@ class App {
     });
   }
 
-  /**
-   * Called when the XRSession has begun. Here we set up our three.js
-   * renderer, scene, and camera and attach our XRWebGLLayer to the
-   * XRSession and kick off the render loop.
-   */
+
   onSessionStarted = async () => {
     // Add the `ar` class to our body, which will hide our 2D components
     document.body.classList.add('ar');
